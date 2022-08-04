a65dfb15eff9298dd6d9653a09ba47504cfb1664
Rampant91 <rampant91@gmail.com>
2022-08-05 01:38:45 +0300

    fix

---
+++ DatabaseAnalysis.WPF/MVVM/Views/Form210.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DatabaseAnalysis.WPF.MVVM.Views
+{
+    public partial class Form210 : UserControl
+    {
+        public Form210()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DatabaseAnalysis.WPF/MVVM/Views/Form29.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DatabaseAnalysis.WPF.MVVM.Views
+{
+    public partial class Form29 : UserControl
+    {
+        public Form29()
+        {
+            InitializeComponent();
+        }
+    }
+}
