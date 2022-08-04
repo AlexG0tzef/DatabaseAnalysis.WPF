6c18a9b8e18426181c59537568cf613f1a5aba03
Rampant91 <rampant91@gmail.com>
2022-08-04 15:28:25 +0300

    Form11-19

---
+++ DatabaseAnalysis.WPF/MVVM/Views/Form19.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DatabaseAnalysis.WPF.MVVM.Views
+{
+    public partial class Form19 : UserControl
+    {
+        public Form19()
+        {
+            InitializeComponent();
+        }
+    }
+}
