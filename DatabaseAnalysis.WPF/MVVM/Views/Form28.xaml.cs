d168a10c52946d704ae8a81699fc846ea654eca7
Rampant91 <rampant91@gmail.com>
2022-08-04 18:00:15 +0300

    28

---
+++ DatabaseAnalysis.WPF/MVVM/Views/Form28.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DatabaseAnalysis.WPF.MVVM.Views
+{
+    public partial class Form28 : UserControl
+    {
+        public Form28()
+        {
+            InitializeComponent();
+        }
+    }
+}
