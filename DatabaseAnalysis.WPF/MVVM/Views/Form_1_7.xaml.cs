3df4c9d3ed9a2d82ae5f6af0e6095710d0fab387
Rampant91 <rampant91@gmail.com>
2022-08-05 03:11:12 +0300

    FinishAllForms

---
+++ DatabaseAnalysis.WPF/MVVM/Views/Form_1_7.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DatabaseAnalysis.WPF.MVVM.Views
+{
+    public partial class Form_1_7 : UserControl
+    {
+        public Form_1_7()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DatabaseAnalysis.WPF/MVVM/Views/Form_2_12.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace DatabaseAnalysis.WPF.MVVM.Views
+{
+    public partial class Form_2_12 : UserControl
+    {
+        public Form_2_12()
+        {
+            InitializeComponent();
+        }
+    }
+}
