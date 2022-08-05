a6b263e9bc6c8923bd948a0d1cb1940f64790f20
Alex Götzef <g0tzef@yandex.ru>
2022-08-05 13:34:42 +0300

    fix

--- DatabaseAnalysis.WPF/Commands/AsyncCommands/GetReportAsyncCommand.cs
+++ DatabaseAnalysis.WPF/Commands/AsyncCommands/GetReportAsyncCommand.cs
@@ -1,5 +1,6 @@
 ﻿using DatabaseAnalysis.WPF.DBAPIFactory;
 using DatabaseAnalysis.WPF.MVVM.ViewModels;
+using DatabaseAnalysis.WPF.Storages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +18,21 @@ namespace DatabaseAnalysis.WPF.Commands.AsyncCommands
         }
         public override async Task AsyncExecute(object? parameter)
         {
-            var api = new EssanceMethods.APIFactory<DatabaseAnalysis.WPF.FireBird.Report>();
-            var rep = await api.GetAsync(Convert.ToInt32(parameter));
+
+            FireBird.Report? rep;
+            var reps = ReportsStorge.ReportsStorage.Where(x => x.Report_Collection.Where(x => x.Id == Convert.ToInt32(parameter)).Count() != 0).FirstOrDefault();
+            var checkedRep = reps.Report_Collection.Where(x => x.Id == Convert.ToInt32(parameter)).FirstOrDefault();
+            if (checkedRep.Rows == null)
+            {
+                var api = new EssanceMethods.APIFactory<DatabaseAnalysis.WPF.FireBird.Report>();
+                rep = await api.GetAsync(Convert.ToInt32(parameter));
+                reps.Report_Collection.Remove(reps.Report_Collection.Where(x => x.Order == rep.Order).FirstOrDefault());
+                reps.Report_Collection.Add(rep);
+            }
+            else
+            {
+                rep = checkedRep;
+            }
 
             if (rep != null)
             {
