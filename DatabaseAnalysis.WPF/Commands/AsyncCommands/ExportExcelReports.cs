7033c90c35786670e4d51605bced85e29c4c5172
Alex Götzef <g0tzef@yandex.ru>
2022-08-08 07:38:15 +0300

    fix

--- DatabaseAnalysis.WPF/Commands/AsyncCommands/ExportExcelReports.cs
+++ DatabaseAnalysis.WPF/Commands/AsyncCommands/ExportExcelReports.cs
@@ -22,64 +22,128 @@ namespace DatabaseAnalysis.WPF.Commands.AsyncCommands
         }
         public override async Task AsyncExecute(object? parameter)
         {
-            SaveFileDialog dial = new();
-            dial.Filter = "Excel | *.xlsx";
-            var saveExcel = dial.ShowDialog(Application.Current.MainWindow);
-            if ((bool)saveExcel!)
+            if (Convert.ToInt32(parameter) == 1)
             {
-                var path = dial.FileName;
-                if (!path.Contains(".xlsx"))
+                SaveFileDialog dial = new();
+                dial.Filter = "Excel | *.xlsx";
+                var saveExcel = dial.ShowDialog(Application.Current.MainWindow);
+                if ((bool)saveExcel!)
                 {
-                    path += ".xlsx";
-                }
-                if (File.Exists(path))
-                {
-                    File.Delete(path);
-                }
-                using (ExcelPackage excelPackage = new ExcelPackage(new FileInfo(path)))
-                {
-                    excelPackage.Workbook.Properties.Author = "RAO_APP";
-                    excelPackage.Workbook.Properties.Title = "Report";
-                    excelPackage.Workbook.Properties.Created = DateTime.Now;
-
-                    if (_navigator.CurrentViewModel is OperReportsViewModel)
+                    var path = dial.FileName;
+                    if (!path.Contains(".xlsx"))
                     {
-                        OperReportsViewModel operReportsViewModel = (OperReportsViewModel)_navigator.CurrentViewModel;
-                        ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Список всех форм 1");
-                        worksheet.Cells[1, 1].Value = "Рег.№";
-                        worksheet.Cells[1, 2].Value = "ОКПО";
-                        worksheet.Cells[1, 3].Value = "Форма";
-                        worksheet.Cells[1, 4].Value = "Дата начала";
-                        worksheet.Cells[1, 5].Value = "Дата конца";
-                        worksheet.Cells[1, 6].Value = "Номер кор.";
-
-                        var lst = new List<DatabaseAnalysis.WPF.FireBird.Reports>();
-
-                        foreach (DatabaseAnalysis.WPF.FireBird.Reports item in operReportsViewModel.Reports!)
-                        {
-                            if (item.Master_DB.FormNum_DB.Split('.')[0] == "1")
-                            {
-                                lst.Add(item);
-                            }
-                        }
-
-                        var row = 2;
-                        foreach (DatabaseAnalysis.WPF.FireBird.Reports reps in lst)
-                        {
-                            foreach (DatabaseAnalysis.WPF.FireBird.Report rep in reps.Report_Collection)
-                            {
-                                worksheet.Cells[row, 1].Value = reps.Master.RegNoRep.Value;
-                                worksheet.Cells[row, 2].Value = reps.Master.OkpoRep.Value;
-                                worksheet.Cells[row, 3].Value = rep.FormNum_DB;
-                                worksheet.Cells[row, 4].Value = rep.StartPeriod_DB;
-                                worksheet.Cells[row, 5].Value = rep.EndPeriod_DB;
-                                worksheet.Cells[row, 6].Value = rep.CorrectionNumber_DB;
-                                row++;
-                            }
-                        }
+                        path += ".xlsx";
                     }
-                    excelPackage.Save();
-                    MessageBox.Show($"Выгрузка \"Всех форм 1\", сохранена по пути {path}");
+                    if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+                    using (ExcelPackage excelPackage = new ExcelPackage(new FileInfo(path)))
+                    {
+                        excelPackage.Workbook.Properties.Author = "RAO_APP";
+                        excelPackage.Workbook.Properties.Title = "Report";
+                        excelPackage.Workbook.Properties.Created = DateTime.Now;
+
+                        if (_navigator.CurrentViewModel is OperReportsViewModel)
+                        {
+                            OperReportsViewModel operReportsViewModel = (OperReportsViewModel)_navigator.CurrentViewModel;
+                            ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Список всех форм 1");
+                            worksheet.Cells[1, 1].Value = "Рег.№";
+                            worksheet.Cells[1, 2].Value = "ОКПО";
+                            worksheet.Cells[1, 3].Value = "Форма";
+                            worksheet.Cells[1, 4].Value = "Дата начала";
+                            worksheet.Cells[1, 5].Value = "Дата конца";
+                            worksheet.Cells[1, 6].Value = "Номер кор.";
+
+                            var lst = new List<DatabaseAnalysis.WPF.FireBird.Reports>();
+
+                            foreach (DatabaseAnalysis.WPF.FireBird.Reports item in operReportsViewModel.Reports!)
+                            {
+                                if (item.Master_DB.FormNum_DB.Split('.')[0] == "1")
+                                {
+                                    lst.Add(item);
+                                }
+                            }
+
+                            var row = 2;
+                            foreach (DatabaseAnalysis.WPF.FireBird.Reports reps in lst)
+                            {
+                                foreach (DatabaseAnalysis.WPF.FireBird.Report rep in reps.Report_Collection)
+                                {
+                                    worksheet.Cells[row, 1].Value = reps.Master.RegNoRep.Value;
+                                    worksheet.Cells[row, 2].Value = reps.Master.OkpoRep.Value;
+                                    worksheet.Cells[row, 3].Value = rep.FormNum_DB;
+                                    worksheet.Cells[row, 4].Value = rep.StartPeriod_DB;
+                                    worksheet.Cells[row, 5].Value = rep.EndPeriod_DB;
+                                    worksheet.Cells[row, 6].Value = rep.CorrectionNumber_DB;
+                                    row++;
+                                }
+                            }
+                        }
+                        excelPackage.Save();
+                        MessageBox.Show($"Выгрузка \"Всех форм 1\", сохранена по пути {path}");
+                    }
+                }
+            }
+            if (Convert.ToInt32(parameter) == 2)
+            {
+                SaveFileDialog dial = new();
+                dial.Filter = "Excel | *.xlsx";
+                var saveExcel = dial.ShowDialog(Application.Current.MainWindow);
+                if ((bool)saveExcel!)
+                {
+                    var path = dial.FileName;
+                    if (!path.Contains(".xlsx"))
+                    {
+                        path += ".xlsx";
+                    }
+                    if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+                    using (ExcelPackage excelPackage = new ExcelPackage(new FileInfo(path)))
+                    {
+                        excelPackage.Workbook.Properties.Author = "RAO_APP";
+                        excelPackage.Workbook.Properties.Title = "Report";
+                        excelPackage.Workbook.Properties.Created = DateTime.Now;
+
+                        if (_navigator.CurrentViewModel is OperReportsViewModel)
+                        {
+                            OperReportsViewModel operReportsViewModel = (OperReportsViewModel)_navigator.CurrentViewModel;
+                            ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("Список всех форм 2");
+                            worksheet.Cells[1, 1].Value = "Рег.№";
+                            worksheet.Cells[1, 2].Value = "ОКПО";
+                            worksheet.Cells[1, 3].Value = "Форма";
+                            worksheet.Cells[1, 4].Value = "Отчетный год";
+                            worksheet.Cells[1, 5].Value = "Номер кор.";
+
+                            var lst = new List<DatabaseAnalysis.WPF.FireBird.Reports>();
+
+                            foreach (DatabaseAnalysis.WPF.FireBird.Reports item in operReportsViewModel.Reports!)
+                            {
+                                if (item.Master_DB.FormNum_DB.Split('.')[0] == "2")
+                                {
+                                    lst.Add(item);
+                                }
+                            }
+
+                            var row = 2;
+                            foreach (DatabaseAnalysis.WPF.FireBird.Reports reps in lst)
+                            {
+                                foreach (DatabaseAnalysis.WPF.FireBird.Report rep in reps.Report_Collection)
+                                {
+                                    worksheet.Cells[row, 1].Value = reps.Master.RegNoRep.Value;
+                                    worksheet.Cells[row, 2].Value = reps.Master.OkpoRep.Value;
+                                    worksheet.Cells[row, 3].Value = rep.FormNum_DB;
+                                    worksheet.Cells[row, 4].Value = rep.Year_DB;
+                                    worksheet.Cells[row, 5].Value = rep.CorrectionNumber_DB;
+                                    row++;
+                                }
+                            }
+                        }
+                        excelPackage.Save();
+                        MessageBox.Show($"Выгрузка \"Всех форм 2\", сохранена по пути {path}");
+                    }
                 }
             }
         }
