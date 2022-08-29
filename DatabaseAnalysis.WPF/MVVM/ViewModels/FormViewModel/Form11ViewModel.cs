ee10d5647d425f9c5482079f4e9b0440fa62b68d
Rampant91 <rampant91@gmail.com>
2022-08-29 08:43:11 +0300

    GetReport to ReportsStorge

--- DatabaseAnalysis.WPF/MVVM/ViewModels/FormViewModel/Form11ViewModel.cs
+++ DatabaseAnalysis.WPF/MVVM/ViewModels/FormViewModel/Form11ViewModel.cs
@@ -1,8 +1,6 @@
-﻿using DatabaseAnalysis.WPF.Commands.AsyncCommands;
-using DatabaseAnalysis.WPF.State.NavigationForm;
+﻿using DatabaseAnalysis.WPF.State.NavigationForm;
 using DatabaseAnalysis.WPF.Storages;
 using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace DatabaseAnalysis.WPF.MVVM.ViewModels.FormViewModel
 {
@@ -23,10 +21,8 @@ namespace DatabaseAnalysis.WPF.MVVM.ViewModels.FormViewModel
         public Form11ViewModel(NavigatorForm navigation, int id, FormsViewModel _formsViewModel)
         {
             _formsViewModel.ValueBar = 50;
-            //var myTask = Task.Factory.StartNew(async () => await ReportsStorge.GetReport(id, this));
-            //await myTask;
-            ICommand GetReport = new GetReportAsyncCommand(this);
-            GetReport.Execute(id);
+            Task myTask = Task.Factory.StartNew(async () => await ReportsStorge.GetReport(id, this));
+            myTask.Wait();
             _formsViewModel.ValueBar = 80;
         }
     }
