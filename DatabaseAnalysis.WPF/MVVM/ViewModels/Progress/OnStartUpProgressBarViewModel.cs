996ba3cf7d41c93b1dcc7a405e965fad5dc9ffef
Rampant91 <rampant91@gmail.com>
2023-06-06 16:51:14 +0300

    teeeeeeeeeeeeest

--- DatabaseAnalysis.WPF/MVVM/ViewModels/Progress/OnStartUpProgressBarViewModel.cs
+++ DatabaseAnalysis.WPF/MVVM/ViewModels/Progress/OnStartUpProgressBarViewModel.cs
@@ -17,68 +17,71 @@ namespace DatabaseAnalysis.WPF.MVVM.ViewModels.Progress
             backgroundWorker.BackgroundWorker(() =>
             {
                 ServiceExtension.LoggerManager.CreateFile("ApplicationLogs.log");
-#if DEBUG
-                StaticConfiguration.DBOperPath = GetLocalCopy(@"C:\RAO\t\OPER", DB_Type.OperDB);
-                StaticConfiguration.DBYearPath = GetLocalCopy(@"C:\RAO\t\YEAR", DB_Type.AnnualDB);
-#else
-                StaticConfiguration.DBOperPath = GetLocalCopy(@"W:\Оперативная отчётность\1-13\Архив базы", DB_Type.OperDB);
-                StaticConfiguration.DBYearPath = GetLocalCopy(@"W:\Годовая отчётность\1-13\БД", DB_Type.AnnualDB);
-#endif
+                var raoDirectory = new DirectoryInfo(Path.Combine(Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System))!, "RAO"));
+                var filesList = raoDirectory
+                    .GetFiles("*.*", SearchOption.TopDirectoryOnly)
+                    .Where(x => x.Name.ToLower().EndsWith(".raodb"))
+                    .OrderByDescending((x => x.LastWriteTime))
+                    .ToList();
+                StaticConfiguration.DBOperPath = filesList.First().FullName;
             }, () =>
             {
-                Application.Current.MainWindow = new MainWindow() { DataContext = new MainWindowViewModel() };
+                Application.Current.MainWindow = new MainWindow { DataContext = new MainWindowViewModel() };
                 Application.Current.MainWindow.Show();
                 startProgressBar.Close();
             });
         }
 
-        #region GetLocalCopy
-        private static string GetLocalCopy(string originDBPath, DB_Type dbType)
-        {
-            string localDBPath = "";
-            string localDBFullPath = "";
-            string msg = "";
-            if (dbType == DB_Type.OperDB)
-            {
-                localDBPath = @"C:\RAO\t\OPER";
-                localDBFullPath = localDBPath + @"\OPER.RAODB";
-                msg = "Файл оперативной отчетности отсутствует в директории";
-            }
-            if (dbType == DB_Type.AnnualDB)
-            {
-                localDBPath = @"C:\RAO\t\YEAR";
-                localDBFullPath = localDBPath + @"\YEAR.RAODB";
-                msg = "Файл годовой отчетности отсутствует в директории";
-            }
-            if (localDBPath.Equals(originDBPath) && File.Exists(localDBFullPath))
-                return localDBFullPath;
-            Directory.CreateDirectory(localDBPath!);
-            DirectoryInfo originDirectoryInfo = new(originDBPath);
-            try
-            {
-                var LastDBFile = originDirectoryInfo.GetFiles("*.*", SearchOption.TopDirectoryOnly)
-                    .Where(x => x.Name.EndsWith(".RAODB"))
-                    .OrderByDescending(x => x.LastWriteTime)
-                    .FirstOrDefault();
-                if (File.Exists(localDBFullPath))
-                    File.Delete(localDBFullPath);
-                File.Copy(LastDBFile!.FullName, localDBFullPath);
-            }
-            catch (Exception ex)
-            {
-                ServiceExtension.LoggerManager.Error($"{msg} {originDBPath}\n{ex}");
-                #region MessageDBMissing
-                MessageBoxResult result = MessageBox.Show(
-                    $"{msg} {originDBPath}",
-                    "Ошибка доступа к базе данных",
-                    MessageBoxButton.OK,
-                     MessageBoxImage.Error);
-                if (result == MessageBoxResult.OK)
-                    Environment.Exit(0);
-                #endregion
-            }
-            return localDBFullPath;
-        } 
-        #endregion
+        //#region GetLocalCopy
+        //private static string GetLocalCopy(string originDBPath, DB_Type dbType)
+        //{
+        //    string localDBPath = "";
+        //    string localDBFullPath = "";
+        //    string msg = "";
+        //    if (dbType == DB_Type.OperDB)
+        //    {
+        //        localDBPath = @"C:\RAO\t\OPER";
+        //        localDBFullPath = localDBPath + @"\OPER.RAODB";
+        //        msg = "Файл оперативной отчетности отсутствует в директории";
+        //    }
+        //    if (dbType == DB_Type.AnnualDB)
+        //    {
+        //        localDBPath = @"C:\RAO\t\YEAR";
+        //        localDBFullPath = localDBPath + @"\YEAR.RAODB";
+        //        msg = "Файл годовой отчетности отсутствует в директории";
+        //    }
+        //    if (localDBPath.Equals(originDBPath) && File.Exists(localDBFullPath))
+        //        return localDBFullPath;
+        //    Directory.CreateDirectory(localDBPath!);
+        //    DirectoryInfo originDirectoryInfo = new(originDBPath);
+        //    var lastDbFile = originDirectoryInfo
+        //        .GetFiles("*.*", SearchOption.TopDirectoryOnly)
+        //        .Where(x => x.Name.EndsWith(".RAODB"))
+        //        .MaxBy(x => x.LastWriteTime);
+        //    try
+        //    {
+        //        if (File.Exists(localDBFullPath))
+        //            File.Delete(localDBFullPath);
+        //        File.Copy(lastDbFile!.FullName, localDBFullPath);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        ServiceExtension.LoggerManager.Error($"{msg} {originDBPath}\n{ex}");
+
+        //        #region MessageDBMissing
+
+        //        var result = MessageBox.Show(
+        //            $"{msg} {originDBPath}",
+        //            "Ошибка доступа к базе данных",
+        //            MessageBoxButton.OK,
+        //             MessageBoxImage.Error);
+        //        if (result == MessageBoxResult.OK)
+        //            Environment.Exit(0);
+
+        //        #endregion
+        //    }
+        //    return localDBFullPath;
+        //} 
+        //#endregion
     }
 }
